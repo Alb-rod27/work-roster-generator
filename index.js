76cf069cb2391e33ea29a1367a7ae50979570e49
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-21 12:55:56 -0800

    fixed index.js manager prompts, going back to pass all tests

--- index.js
+++ index.js
@@ -2,34 +2,47 @@ const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 
-function addManager()  {
-inquirer
-.prompt({
-    type: 'text',
-    name: 'name',
-    message: "What is the managers name?"
-})
-.then(({ name }) => {
-    this.employee = new Employee(name);
+let teamArray = [];
+
+init(); {
+    addManager();
+    addTeamMembers();
+}
+
+function addManager() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the managers name?'
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'Please enter manager id number.'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'Please enter manager email address.'
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: 'Please enter managers office number.'
+    },
+ ]).then(res => {
+     console.log(res);
+     const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNumber)
+     teamArray.push(manager)
+     addTeamMembers();
+ });
+};
+
+init();
+
     
-});
-
-inquirer
-.prompt({
-    type: 'input',
-    name: 'id',
-    message: 'Please enter employee id number.'
-})
-    .then(({ id }) => {
-        this.employee = new Employee(id);
-    })
-
-inquirer
-.prompt({
-    type: 'input',
-    name: 'email',
-    message: 'Please enter employee email address.'
-})
-
