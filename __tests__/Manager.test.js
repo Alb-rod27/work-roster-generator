9092504cfdbe8fe4148275499490199d164f4b23
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-21 15:37:11 -0800

    manager test fully working

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,31 @@
 const { TestScheduler } = require('jest');
 const Manager = require('../lib/Manager');
 
+const manager = new Manager('name', '1', 'test@test.com', '888-888-8888')
+
 test('creates manager object', () => {
-    const manager = new Manager()
+    expect(manager.name).toBe('name');
+    expect(manager.id).toBe('1');
+    expect(manager.email).toBe('test@test.com');
+    expect(manager.officenumber).toBe('888-888-8888');
+});
+
+test('returns name', () => {
+    expect(manager.getName()).toBe('name')
+});
+
+test('returns id', () => {
+    expect(manager.getId()).toBe('1')
+})
+
+test('returns email', () => {
+    expect(manager.getEmail()).toBe('test@test.com')
+})
+
+test('returns role', () => {
+    expect(manager.getRole()).toBe('Manager')
+})
+
+test('returns office number', () => {
+    expect(manager.getOfficeNumber()).toBe('888-888-8888')
 })
